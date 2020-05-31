1f27fd4fea0cef1af30585abb14dd530882cb989
liumin53 <gavin.liu@mail.utoronto.ca>
2020-05-31 15:15:53 -0400

    added procFile

--- MobilePriceCalculator/server/index.js
+++ MobilePriceCalculator/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 const data = require("./itemPrice.json");
 const tax = 1.13;
 var bodyParser = require("body-parser");
